32fa7aedc437cdc820a23ade7de5cec7dc1f2cfa
Ben Woodard <benjaminwoodard93@gmail.com>
2023-12-04 21:22:47 -0500

    renamed Runnable class to TeslaSalesReportApplication

--- src/com/coderscampus/benWoodardAssignment6/TeslaSalesReportApplication.java
+++ src/com/coderscampus/benWoodardAssignment6/TeslaSalesReportApplication.java
@@ -3,22 +3,14 @@ package com.coderscampus.benWoodardAssignment6;
 import java.text.ParseException;
 import java.util.List;
 
-public class Runnable {
+public class TeslaSalesReportApplication {
 
 	public static void main(String[] args) throws ParseException {
-//		FileService fileService = new FileService();
-//		SalesDataService salesDataService = new SalesDataService();
-//		List<CarSales> poop = fileService.readCarFile("./teslaDataCSV/model3.csv");
-//		
-//		salesDataService.bestSalesMonth(poop, "Model poop");
 
 		ReportsService reportsService = new ReportsService();
 		reportsService.generateReport("./teslaDataCSV/model3.csv", "Model 3");
 		reportsService.generateReport("./teslaDataCSV/modelS.csv", "Model S");
 		reportsService.generateReport("./teslaDataCSV/modelX.csv", "Model X");
-
-	
-
 	}
 
 }
